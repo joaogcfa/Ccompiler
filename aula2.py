07017a3188dcd8ee2c937d16c96dbd8f8df93c9e
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2022-03-13 14:43:51 -0300

    divisao e mult implementado

--- aula2.py
+++ aula2.py
@@ -35,16 +35,6 @@ class Tokenizer:
             self.actual = Token('-', "MINUS")
             return self.actual
 
-        elif self.origin[self.position] == '*':
-            self.position += 1
-            self.actual = Token('*', "MULT")
-            return self.actual
-
-        elif self.origin[self.position] == '/':
-            self.position += 1
-            self.actual = Token('/', "DIV")
-            return self.actual
-
         elif self.origin[self.position].isdigit():
             candidato = self.origin[self.position]
             self.position += 1
@@ -69,60 +59,34 @@ class Parser:
     tokens = None
 
     def parseExpression():
-        resultado = Parser.parseTerm()
-
-        while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS":
-            if Parser.tokens.actual.type == "PLUS":
-                Parser.tokens.selectNext()
-                resultado += Parser.parseTerm()
-            elif Parser.tokens.actual.type == "MINUS":
-                Parser.tokens.selectNext()
-                resultado -= Parser.parseTerm()
-            else:
-                raise ValueError
-        return resultado
-
-    def parseTerm():
-
+        Parser.tokens.selectNext()
         resultado = None
         if Parser.tokens.actual.type == "INT":
             resultado = Parser.tokens.actual.value
             Parser.tokens.selectNext()
-
-            while Parser.tokens.actual.type == "MULT" or Parser.tokens.actual.type == "DIV":
-                if Parser.tokens.actual.type == "MULT":
+            while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS":
+                if Parser.tokens.actual.type == "PLUS":
                     Parser.tokens.selectNext()
                     if Parser.tokens.actual.type == "INT":
-                        resultado *= Parser.tokens.actual.value
-                        print(resultado)
+                        resultado += Parser.tokens.actual.value
                     else:
-                        # print("1")
                         raise ValueError
-                elif Parser.tokens.actual.type == "DIV":
+                if Parser.tokens.actual.type == "MINUS":
                     Parser.tokens.selectNext()
                     if Parser.tokens.actual.type == "INT":
-                        resultado /= Parser.tokens.actual.value
+                        resultado -= Parser.tokens.actual.value
                     else:
-                        # print("2")
                         raise ValueError
                 else:
-                    # print("3")
                     raise ValueError
                 Parser.tokens.selectNext()
             return resultado
         else:
-            # print("4")
             raise ValueError
 
     def run(code):
         Parser.tokens = Tokenizer(code)
-        Parser.tokens.selectNext()
         return Parser.parseExpression()
-
-
-# class PrePro:
-#     def filter():
-#         return code
 
 
 arg = sys.argv[1]
