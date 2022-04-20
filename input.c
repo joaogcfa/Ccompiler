fd0b3b2d7a6379c7e6da242afcb9b1057c4ed2f4
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2022-04-20 13:24:02 -0300

    finalizado

--- input.c
+++ input.c
@@ -1,1 +1,23 @@
-21 + 21
+{
+    x = 3;
+    y = 2;
+    x_2 = 7;
+    y_2 = 5;
+
+    z = x + y;
+    z_2 = x_2 + y_2;
+    printf(x);
+    printf(z);
+    printf(z_2);
+    printf(x - -y);
+
+    w = (x + y) / y_2;
+    printf(/* bla */ w /* bla */);
+    /*bla bla
+    bla
+    bla */
+
+    printf(w + x);
+    b = w + x_2;
+    printf(b + w + z);
+}
