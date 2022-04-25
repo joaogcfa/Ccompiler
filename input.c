7a2c30ec1fbe060ddf5e9a30b51f6ab3bdfce1a0
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2022-04-25 10:35:43 -0300

    alterando mais para pegar os testes

--- input.c
+++ input.c
@@ -1,21 +1,13 @@
 {
     x = 1;
-    y = 2;
-    x_3 = 3;
-    y_4 = 4;
 
-    z = x + y;
-    z_2 = x_3 + y_4;
     printf(x);
-    /*aaaa
-    a */
-    printf(y);
-    printf(z);
-    printf(x_3);
-    printf(y_4);
-    printf(z_2);
-    printf(x - -y);
-
-    w = (x + y) / y_4 * x_3;
-    printf(/* bla */ w /* bla */);
+    ;
+    ;
+    ;
+    ;
+    ;
+    ;
+    ;
+    ;
 }
--- variaveis.py
+++ variaveis.py
@@ -1,3 +1,4 @@
+from curses.ascii import isdigit
 from select import select
 import sys
 
@@ -9,6 +10,7 @@ class Token:
 
 
 class Tokenizer:
+
     def __init__(self, origin):
         self.origin = origin
         self.position = 0
@@ -16,14 +18,17 @@ class Tokenizer:
 
     def selectNext(self):
 
-        if self.position < len(self.origin) and self.origin[self.position] == ' ':
+        if self.position < len(self.origin) and self.origin[self.position] in [' ', "\n"]:
             self.position += 1
-            while self.position < len(self.origin) and self.origin[self.position] == ' ':
+            while self.position < len(self.origin) and self.origin[self.position] in [' ', "\n"]:
                 self.position += 1
 
-        if self.position >= len(self.origin):
+        if self.position == len(self.origin):
             self.actual = Token("", "EOF")
             return self.actual
+
+        elif self.position > len(self.origin):
+            raise Exception("eof passou")
 
         if self.origin[self.position] == '+':
             self.position += 1
@@ -55,6 +60,26 @@ class Tokenizer:
             self.actual = Token(')', "CLOSE_PAR")
             return self.actual
 
+        elif self.origin[self.position] == '{':
+            self.position += 1
+            self.actual = Token('{', "OPEN_BRACK")
+            return self.actual
+
+        elif self.origin[self.position] == '}':
+            self.position += 1
+            self.actual = Token('}', "CLOSE_BRACK")
+            return self.actual
+
+        elif self.origin[self.position] == ';':
+            self.position += 1
+            self.actual = Token(';', "SEMI_COLON")
+            return self.actual
+
+        elif self.origin[self.position] == '=':
+            self.position += 1
+            self.actual = Token('=', "EQUALS")
+            return self.actual
+
         elif self.origin[self.position].isdigit():
             candidato = self.origin[self.position]
             self.position += 1
@@ -67,8 +92,27 @@ class Tokenizer:
                     self.position += 1
                 else:
                     isInt = False
-
             self.actual = Token(int(candidato), "INT")
+            return self.actual
+
+        elif self.origin[self.position].isalpha():
+            candidato = self.origin[self.position]
+            self.position += 1
+            isChar = True
+            while isChar:
+                if self.position == len(self.origin):
+                    isChar = False
+                elif self.origin[self.position].isalpha() or self.origin[self.position] == '_' or self.origin[self.position].isdigit():
+                    candidato += self.origin[self.position]
+                    self.position += 1
+                else:
+                    isChar = False
+            if candidato == "printf":
+                # print("print")
+                self.actual = Token(candidato, "PRINT")
+            else:
+                # print("ident: ", candidato)
+                self.actual = Token(candidato, "IDENT")
             return self.actual
 
         else:
@@ -78,9 +122,64 @@ class Tokenizer:
 class Parser:
     tokens = None
 
+    def parseBlock():
+        Node = Block("", [])
+        # print(Parser.tokens.actual.value)
+        if Parser.tokens.actual.type == "OPEN_BRACK":
+            Parser.tokens.selectNext()
+            while Parser.tokens.actual.type != "CLOSE_BRACK":
+                # print(Parser.tokens.actual.value)
+                Node.children.append(Parser.parseStatement())
+            Parser.tokens.selectNext()
+        else:
+            raise ValueError
+        return Node
+
+    def parseStatement():
+        # print("Statement", Parser.tokens.actual.value)
+        Node = None
+        if Parser.tokens.actual.type == "IDENT":
+            Node = Identifier(Parser.tokens.actual.value, [])
+            Parser.tokens.selectNext()
+            if Parser.tokens.actual.type == "EQUALS":
+                Parser.tokens.selectNext()
+                Node = Assignment("", [Node, Parser.parseExpression()])
+            if Parser.tokens.actual.type == "SEMI_COLON":
+                Parser.tokens.selectNext()
+                return Node
+            else:
+                raise Exception("FALTOU PONTO E VIRGULA OU IGUAL")
+
+        elif Parser.tokens.actual.type == "PRINT":
+            Parser.tokens.selectNext()
+            # print("145", Parser.tokens.actual.value)
+            if Parser.tokens.actual.type == "OPEN_PAR":
+                Parser.tokens.selectNext()
+                # print("147", Parser.tokens.actual.value)
+                Node = Print("", [Parser.parseExpression()])
+                if Parser.tokens.actual.type == "CLOSE_PAR":
+                    Parser.tokens.selectNext()
+                    if Parser.tokens.actual.type == "SEMI_COLON":
+                        Parser.tokens.selectNext()
+                        return Node
+                    else:
+                        raise Exception("PONTO E VIRGULA DO PRINT")
+                else:
+                    raise Exception("FALTOU FECHA PARENTESES DO PRINT")
+            else:
+                raise Exception("ABRE PARENTESES DO PRINT")
+        elif Parser.tokens.actual.type == "SEMI_COLON":
+            Parser.tokens.selectNext()
+            Node = NoOp("", [])
+            return Node
+        else:
+            raise ValueError
+
     def parseExpression():
+        # print("EXPRESSION", Parser.tokens.actual.value)
         # print("Expression")
         Node = Parser.parseTerm()
+        # print("BACK TO EXPRESSION", Parser.tokens.actual.value)
 
         while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS":
             if Parser.tokens.actual.type == "PLUS":
@@ -98,7 +197,7 @@ class Parser:
         return Node
 
     def parseTerm():
-        # print("Term")
+        # print("Term", Parser.tokens.actual.value)
         Node = Parser.parseFactor()
 
         while Parser.tokens.actual.type == "MULT" or Parser.tokens.actual.type == "DIV":
@@ -119,13 +218,21 @@ class Parser:
     def parseFactor():
         # print("Factor")
         Node = None
-
+        # print("FACTOR", Parser.tokens.actual.value)
         if Parser.tokens.actual.type == "INT":
             # print("INT")
             # resultado = Parser.tokens.actual.value
             # print("tipo: ", type(Parser.tokens.actual.value))
             Node = IntVal(Parser.tokens.actual.value, None)
             Parser.tokens.selectNext()
+            # print("AFTER INT", Parser.tokens.actual.value)
+
+        elif Parser.tokens.actual.type == "IDENT":
+            # print("soma2")
+            # resultado = Parser.parseFactor()
+            Node = Identifier(Parser.tokens.actual.value, [])
+            Parser.tokens.selectNext()
+            # print("AFTER IDENT", Parser.tokens.actual.value)
 
         elif Parser.tokens.actual.type == "PLUS":
             # print("soma2")
@@ -141,6 +248,8 @@ class Parser:
         elif Parser.tokens.actual.type == "OPEN_PAR":
             # print("par")
             Parser.tokens.selectNext()
+            # print(Parser.tokens.actual.value)
+
             Node = Parser.parseExpression()
             if Parser.tokens.actual.type == "CLOSE_PAR":
                 # print("fechapar")
@@ -158,8 +267,9 @@ class Parser:
         # print(code_filtrado)
         file.close()
         Parser.tokens = Tokenizer(code_filtrado)
+        Parser.table = SymbolTable()
         Parser.tokens.selectNext()
-        resultado = Parser.parseExpression()
+        resultado = Parser.parseBlock()
         if Parser.tokens.actual.type != "EOF":
             raise ValueError
         return resultado.Evaluate()
@@ -197,12 +307,37 @@ class Node:
         pass
 
 
+class Assignment(Node):
+
+    def Evaluate(self):
+        SymbolTable.Setter(
+            self.children[0], self.children[1].Evaluate())
+
+
+class Block(Node):
+    def Evaluate(self):
+        for child in self.children:
+            child.Evaluate()
+
+
+class Print(Node):
+    def Evaluate(self):
+        children = self.children[0].Evaluate()
+        print(children)
+
+
+class Identifier(Node):
+
+    def Evaluate(self):
+        return SymbolTable.Getter(self.value)
+
+
 class BinOp(Node):
 
     def Evaluate(self):
-        # print("BinOp")
         right = self.children[0].Evaluate()
         left = self.children[1].Evaluate()
+        # print(right, left, "right and left")
         if self.value == "PLUS":
             return right + left
         if self.value == "MINUS":
@@ -237,6 +372,21 @@ class NoOp(Node):
         pass
 
 
+class SymbolTable:
+
+    table = {}
+
+    def Getter(chave):
+        # print("chave: ", chave)
+        # print("chave com valor: ", SymbolTable.table[chave])
+        if chave in dict.keys(SymbolTable.table):
+            return SymbolTable.table[chave]
+
+    def Setter(chave, valor):
+        SymbolTable.table[chave.value] = valor
+        # print("table", SymbolTable.table)
+
+
 arg = sys.argv[1]
-print(Parser.run(arg))
+Parser.run(arg)
 # print(Parser.run("/* a */ 1 /* b */"))
